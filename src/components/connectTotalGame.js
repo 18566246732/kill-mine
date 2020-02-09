bd28a1795e046377d6717ea04c4fad927f78367a
ivanliu <ivanliu@wezhuiyi.com>
2020-02-10 07:59:04 +0800

    fix: remove useless code

--- src/components/connectTotalGame.js
+++
@@ -1,8 +0,0 @@
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import action from 'action.js'
-
-export default connect(
-  state => state.totalGame,
-  dispatch => bindActionCreators(action, dispatch)
-)
